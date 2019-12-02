d3dcf3d0d762909bc130274708d7690a4b4c33a9
Amanda Reges <reges.amanda1@gmail.com>
2019-12-02 12:49:54 -0200

    ex003.py

---
+++ PycharmProjects/exercicios/ex003.py
@@ -0,0 +1,5 @@
+n1 = float(input("Digite um valor: "))
+n2 = float(input("Digite outro valor: "))
+s = n1 + n2
+print("A soma entre {} e {} é igual a {}!".format(n1, n2, s))
+# Se fosse somente com numeros inteiros colocaria (int)
