0ff17fd1dded0ee3557ca1f9e4f2672fbb0e4b2d
Amanda Reges <reges.amanda1@gmail.com>
2019-12-02 12:55:40 -0200

    ex008.py

---
+++ PycharmProjects/exercicios/ex008.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia um valor em metros e o exiba convertot em centimetro e milimetro
+# escala: km hm dam m dm cm mm
+medida = float(input("Uma distância em metros: "))
+cm = medida * 100
+mm = * 1000
+print("A medida de {}m correnponde a {}cm e {}mm".format(medida, cm, mm))
